3739da9607f4e395d7752ed35464ae089b6e07df
jovanohks <jovanoh400@gmail.com>
2025-10-25 16:23:08 +0800

    Update testquestion1.js

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+let bird
+let background, birdImg;
+
+function preload(){
+    // write code to preload bird.png and background.png
+    // refer to assets folder
+    background = loadImage('assets/background.png'); //background
+    birdImg = loadImage('assets/bird.png'); //preload bird image
+}
+
+function setup(){
+    new Canvas(400, 600);
+    // write code to create sprite and load bird image on sprite
+    bird = new Sprite(); // create bird sprite
+    bird.img = birdImg; 
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
 
